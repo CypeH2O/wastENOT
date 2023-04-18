bae5f14bf5f627c41d97020bb48f097199b26378
kust <cug.s.il@yandex.ru>
2023-04-18 19:33:27 +0300

    ебануло сцуко но работает

---
+++ app/src/main/java/com/example/opd/ui/Map/MapActivity.java
@@ -0,0 +1,22 @@
+package com.example.opd.ui.Map;
+
+import static android.content.ContentValues.TAG;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+
+import com.example.opd.R;
+
+public class MapActivity {
+//    //код для карты (УДАЧИ ЭТО ЗАПУШИТЬ)
+//    setContentView(R.layout.activity_map);
+//    WebView webView = findViewById(R.id.webView);
+//    if(webView != null){
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.loadUrl("https://regreenpolytech.github.io/map/");
+//    }
+//    else{
+//        Log.e(TAG, "webView is null");
+//    }
+}
