5fcc90044e60fc6c25ec24b273278c6bb86f73bb
AlPakh <alvp2003@yandex.ru>
2023-05-23 13:52:35 +0300

    >Переместил UI карты из PageFragment в MapActivity >Удалил PageFragment >Добавил отступ в диалоговое окно совета >Переименовал activity_msp.xml в activity_map.xml

--- app/src/main/java/com/example/opd/FragmentAdapter.java
+++ app/src/main/java/com/example/opd/FragmentAdapter.java
@@ -1,13 +1,12 @@
 package com.example.opd;
 
-
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.example.opd.ui.Calculator.CalcFragment;
+import com.example.opd.ui.Map.MapActivity;
 import com.example.opd.ui.News.NewsFragment;
 
 public class FragmentAdapter extends FragmentStateAdapter {
@@ -21,7 +20,7 @@ public class FragmentAdapter extends FragmentStateAdapter {
         Fragment resultFragment =null;
         switch (position) {
             case 0:
-                resultFragment = PageFragment.newInstance(position);
+                resultFragment = MapActivity.newInstance(position);
                 break;
             case 1:
                 resultFragment = CalcFragment.newInstance(position);
